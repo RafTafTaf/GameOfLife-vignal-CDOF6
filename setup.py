410ced42330fb33b20ef61307453d12c4e6ff6a8
Diane Verbecq <diane.verbecq@gmail.com>
2025-01-13 17:17:50 +0100

    setup bug fixed

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 from distutils.core import setup
+from setuptools import setup
 setup(
  name='{GameOfLife-vignal-CDOF6}',
  version='1.0',
  author='{RafTafTaf}',
  license='MIT',
  long_description=open('README.md').read(),
+     install_requires=[
+        'numpy',
+    ],
 )
